2a4e3ef4c4165ce15a584041a63cfed9b338d187
Raul Flores <raul.flores@ciberc.com>
2021-06-28 11:28:02 -0600

    se arregla el archivo settings del proyecto netauto, correccion 1

--- netauto/netauto/settings.py
+++ netauto/netauto/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = os.path.join('/staticfiles/')
-STATIC_FILES_DIRS =(os.path.join('staticfiles'))
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS =(os.path.join('staticfiles'),)
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
