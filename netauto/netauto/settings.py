4944e472080e6753b535bbcb6c2b894c4cc88333
Raul Flores <raul.flores@ciberc.com>
2021-07-10 11:32:19 -0600

    Se arreglan los templates para ser 100% bootstrap, se agrega crispy forms al proyecto

--- netauto/netauto/settings.py
+++ netauto/netauto/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'napalmweb'
+    'napalmweb',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,7 @@ STATIC_URL = os.path.join('/static/')
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static'),
 ]
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
